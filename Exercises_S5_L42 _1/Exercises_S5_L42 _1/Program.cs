8c7038ecacbe6cae24d55059367657b098383f80
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 09:39:44 -0500

    exercise - section 5 lesson 42 #1

---
+++ Exercises_S5_L42 _1/Exercises_S5_L42 _1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L42__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number from 1 - 10:");
+            var input = Int32.Parse(Console.ReadLine());
+
+            if (input >= 1 && input <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
