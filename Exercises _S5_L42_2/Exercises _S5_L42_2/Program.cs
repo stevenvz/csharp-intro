a6b9c41b38f5e6be39d4c2a1defd69a21a7ec91d
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 09:44:44 -0500

    exercise - section 5 lesson 42 #2

---
+++ Exercises _S5_L42_2/Exercises _S5_L42_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises__S5_L42_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            var num1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another number:");
+            var num2 = Int32.Parse(Console.ReadLine());
+
+            if (num1 >= num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
