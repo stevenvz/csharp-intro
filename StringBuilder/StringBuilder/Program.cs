fe79f1ba8661d3669c83bcbdfb7c4a56418a7f26
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 16:32:03 -0500

    string builder

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new System.Text.StringBuilder("Hello World");
+
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10)
+                .Replace('-', '+')
+                .Remove(0, 10)
+                .Insert(0, new string('-', 10));
+
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First Char: " + builder[0]);
+        }
+    }
+}
