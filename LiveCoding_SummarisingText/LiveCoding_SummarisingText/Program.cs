29f6c48887840ae302817d7169f19fe71f195ca0
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 16:22:21 -0500

    live coding - summarise text

---
+++ LiveCoding_SummarisingText/LiveCoding_SummarisingText/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCoding_SummarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            var summary = StringUtility.SummariseText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+    }
+}
