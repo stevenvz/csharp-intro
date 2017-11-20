594ffb2dd2ebd1abc8b25df48001dece1ca2c3e2
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 09:18:24 -0500

    conditionals

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("We've got a promotion!");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season.");
+                    break;
+            }
+
+        }
+    }
+}
