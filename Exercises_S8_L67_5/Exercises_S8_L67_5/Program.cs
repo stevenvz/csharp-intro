675682267d53377414c9e75c41ffdf76ee141266
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 18:09:12 -0500

    exercise - section 8 lesson 67 #5

---
+++ Exercises_S8_L67_5/Exercises_S8_L67_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S8_L67_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            var input = Console.ReadLine();
+
+            var count = 0;
+            var textList = new List<char>();
+            var builder = new StringBuilder(input);
+
+            for (int i = 0; i < builder.Length; i++)
+            {
+                textList.Add(builder[i]);
+                if (builder[i] == 'a' ||
+                    builder[i] == 'e' ||
+                    builder[i] == 'i' ||
+                    builder[i] == 'o' ||
+                    builder[i] == 'u')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+
+        }
+    }
+}
