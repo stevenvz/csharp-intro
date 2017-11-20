f194f9c867f71921679daa4eb74a60f6de0d4fba
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 14:38:16 -0500

    exercise - section 6 lesson 55 #2

---
+++ Exercises_S6_L55_2/Exercises_S6_L55_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S6_L55_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+
+            var nameArray = new char[(name.Length)];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                nameArray[name.Length - i - 1] = name[i];
+            }
+
+            var reverse = string.Join("", nameArray);
+            Console.WriteLine(reverse);
+        }
+    }
+}
