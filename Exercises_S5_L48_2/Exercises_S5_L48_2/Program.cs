feb4af35c58ed8c4814c08763dcd7a8e7eaecd6a
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 11:32:15 -0500

    exercise - section 5 lesson 48 #2

---
+++ Exercises_S5_L48_2/Exercises_S5_L48_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L48_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            while (true)
+            {
+                Console.Write("Enter a number, or \"ok\" tp exit: ");
+                var input = Console.ReadLine();
+
+                if (input == "ok")
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+
+                sum += Int32.Parse(input);
+            }
+        }
+    }
+}
