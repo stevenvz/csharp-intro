520ef048cf21323d0b36b3aa3739f784f1acf9f4
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 10:37:34 -0500

    iterations - foreach

---
+++ Iterations_foreach/Iterations_foreach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] {1, 2, 3, 4};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
