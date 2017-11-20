a6c713d97df4a597c1e0fb8a569009a98737483c
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 15:26:28 -0500

    exercise - section 6 lesson 55 #5

---
+++ Exercises_S6_L55_5/Exercises_S6_L55_5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S6_L55_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Enter a series of numbers separated by commas (minimum length 5): ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("Invalid List. Try Again.");
+                }
+                else
+                {
+                    string[] values = input.Split(',');
+
+                    foreach (string item in values)
+                    {
+                        numbers.Add(Int32.Parse(item));
+                    }
+
+                    if (numbers.Count < 5)
+                    {
+                        Console.WriteLine("Invalid List. Try Again.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            numbers.Sort();
+
+            for (var i = 0; i < 3; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
