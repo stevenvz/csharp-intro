85630a1bc5999053c0cc59e9ed42485a4dbeeec7
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 10:10:50 -0500

    exercise - section 5 lesson 42 #4

---
+++ Exercises_S5_L42_4/Exercises_S5_L42_4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L42_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the speed limit:");
+            var speedLimit = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the car's speed:");
+            var carSpeed = Int32.Parse(Console.ReadLine());
+
+            int demerits = (carSpeed - speedLimit) / 5;
+
+            if (demerits <= 0 && carSpeed <= speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else if (demerits >= 0 && demerits <= 12)
+            {
+                Console.WriteLine(demerits);
+            }
+            else
+            {
+                Console.WriteLine("License Suspended");
+            }
+        }
+    }
+}
