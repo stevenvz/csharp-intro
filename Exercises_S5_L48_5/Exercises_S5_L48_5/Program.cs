d95ebfcfe40d66bb31e83d29d1b03baa194c75c0
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 12:37:02 -0500

    exercise - section 5 lesson 48 #5

---
+++ Exercises_S5_L48_5/Exercises_S5_L48_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L48_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a series of numbers separated by commas: ");
+            var input = Console.ReadLine();
+            
+            var max = 0;
+
+            string[] values = input.Split(',');
+
+            foreach (string item in values)
+            {
+                if (Int32.Parse(item) > max)
+                {
+                    max = Int32.Parse(item);
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
