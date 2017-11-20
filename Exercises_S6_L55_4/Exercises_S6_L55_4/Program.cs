f65a1d23e173b36361f48085c6acaeff09c0e608
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 15:09:13 -0500

    exercise - section 6 lesson 55 #4

---
+++ Exercises_S6_L55_4/Exercises_S6_L55_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S6_L55_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            var uniques = new List<int>();
+
+                while (true)
+                {
+                    Console.Write("Enter a number, or \"Quit\" to exit: ");
+                    var input = Console.ReadLine();
+
+                    if (input == "Quit")
+                        break;
+
+                    var number = Int32.Parse(input);
+
+                    if (!numbers.Contains(number))
+                        uniques.Add(number);
+
+                    numbers.Add(number);
+                }
+
+            foreach (var n in uniques)
+                Console.Write(n + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
