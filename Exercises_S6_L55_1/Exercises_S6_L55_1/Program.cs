05560469af70a013b30cc02cd9fe967d2b80e89c
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 14:21:42 -0500

    exercise - section 6 lesson 55 #1

---
+++ Exercises_S6_L55_1/Exercises_S6_L55_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S6_L55_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+
+            while (true)
+            {
+                Console.Write("Enter a name (or just <enter> to stop): ");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input))
+                    break;
+
+                names.Add(input);
+            }
+
+            switch (names.Count)
+            {
+                case 0:
+                    break;
+                case 1:
+                    Console.WriteLine(names[0] + " likes your post");
+                    break;
+                case 2:
+                    Console.WriteLine("{0} and {1} like your post", names[0], names[1]);
+                    break;
+                default:
+                    Console.WriteLine("{0}, {1}, and {2} others like your post", names[0], names[1], (names.Count - 2));
+                    break;
+            }
+            
+        }
+    }
+}
