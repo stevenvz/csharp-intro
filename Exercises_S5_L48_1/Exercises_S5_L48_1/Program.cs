a1dc0702339bd79c3a26441dc0a5568941758722
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 11:24:15 -0500

    exercise - section 5 lesson 48 #1

---
+++ Exercises_S5_L48_1/Exercises_S5_L48_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L48_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
