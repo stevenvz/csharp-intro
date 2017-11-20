3577c7c2ad4e57ec86d3c09149e3778261f1c8eb
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 18:00:25 -0500

    exercise - section 8 lesson 66 #4

---
+++ Exercises_S8_L67_4/Exercises_S8_L67_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S8_L67_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few words separated by a space: ");
+            var input = Console.ReadLine().ToLower();
+
+            var text = input.Split(' ');
+
+            var textList = new List<string>();
+
+            foreach (var item in text)
+            {
+                var word = new StringBuilder(item);
+                word[0] = (char)(word[0] - 32);
+                textList.Add(word.ToString());
+            }
+
+            var final = String.Join(String.Empty, textList.ToArray());
+
+            Console.WriteLine(final);
+
+        }
+    }
+}
