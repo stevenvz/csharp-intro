6e87d4591ac6bcb2e44969d5204b404797053998
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 10:45:35 -0500

    iterations - while

---
+++ Iterations_while/Iterations_while/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterations_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (!String.IsNullOrWhiteSpace(input))
+                {
+                    Console.WriteLine("@Echo: " + input);
+                    continue;
+                }
+
+                break;
+            }
+        }
+    }
+}
