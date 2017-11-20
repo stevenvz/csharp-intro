20dd85adbdc09dc53f96237cf449af58cc37c2fa
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 14:54:52 -0500

    exercise - section 6 lesson 55 #3

---
+++ Exercises_S6_L55_3/Exercises_S6_L55_3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S6_L55_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter a number: ");
+                var number = Int32.Parse(Console.ReadLine());
+
+                while (true)
+                {
+                    if (numbers.Contains(number))
+                    {
+                        Console.Write("Retry: ");
+                        number = Int32.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        numbers.Add(number);
+                        break;
+                    }
+                }
+            }
+
+            numbers.Sort();
+
+            foreach (var n in numbers)
+                Console.Write(n + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
