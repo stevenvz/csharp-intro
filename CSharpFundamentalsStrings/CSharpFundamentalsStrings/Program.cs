cd7d6a5ec2ac0f6b8a2da7775ed766d72b08afaa
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-19 22:52:31 -0500

    strings

---
+++ CSharpFundamentalsStrings/CSharpFundamentalsStrings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentalsStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Steve";
+            var lastName = "Zarrella";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new string[3] {"John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+        }
+    }
+}
