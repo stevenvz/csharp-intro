71713969577754be0f74a3fdd4f0226d55b78577
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-19 20:04:26 -0500

    first few lessons

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+                //throw;
+            }
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+        }
+    }
+}
