81105d07e939cba439821b857b84a6a6fa035d96
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 11:48:04 -0500

    exercise - section 5 lesson 48 #3

---
+++ Exercises_S5_L48_3/Exercises_S5_L48_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L48_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var number = Int64.Parse(Console.ReadLine());
+            Int64 factorial = number;
+
+            for (Int64 i = number - 1; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+        }
+    }
+}
