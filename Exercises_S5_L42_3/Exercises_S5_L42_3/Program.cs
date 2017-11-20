0422a4329b41fe879984341e49435836d63c4d67
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 09:51:39 -0500

    exercise - section 5 lesson 42 #3

---
+++ Exercises_S5_L42_3/Exercises_S5_L42_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_S5_L42_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter image width:");
+            var width = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter image height:");
+            var height = Int32.Parse(Console.ReadLine());
+
+            if (width > height)
+            {
+                Console.WriteLine("The image is landscape.");
+            }
+            else if (width < height)
+            {
+                Console.WriteLine("The image is portrait.");
+            }
+            else
+            {
+                Console.WriteLine("The image is square!");
+            }
+        }
+    }
+}
