bfe324e066ba83401d50757d383b03b76fe45cc4
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 19:58:59 -0500

    exercise - section 9 lesson 75 #1

---
+++ Exercises_S9_L75_1/Exercises_S9_L75_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Exercises_S9_L75_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\data\file.txt";
+
+            if (File.Exists(path))
+            {
+                var content = File.ReadAllText(path);
+                var contentArray = content.Split(' ');
+
+                Console.WriteLine(contentArray.Length);
+            }
+            
+        }
+    }
+}
