08641bffbe6dd5cb618d07d1d981f3d936dbfcf8
Steven Zarrella <stevenvz@users.noreply.github.com>
2017-11-20 20:06:34 -0500

    exercise - section 9 lesson 75 #2

---
+++ Exercises_S9_L75_2/Exercises_S9_L75_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exercises_S9_L75_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = 0;
+            var index = 0;
+            var path = @"C:\data\file.txt";
+
+            if (File.Exists(path))
+            {
+                var content = File.ReadAllText(path);
+                var contentArray = content.Split(' ');
+
+                foreach (var item in contentArray)
+                {
+                    if (item.Length > length)
+                    {
+                        length = item.Length;
+                        index = Array.IndexOf(contentArray, item);
+                    }
+                }
+
+                Console.WriteLine(contentArray[index]);
+            }
+        }
+    }
+}
